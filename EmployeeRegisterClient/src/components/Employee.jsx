87e79e87c21d7d591e8cb1a16c68470957f05dd2
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-08-05 00:31:01 +0700

    EmployeeRegisterClient - Form Validation with File Uploader

--- EmployeeRegisterClient/src/components/Employee.jsx
+++ EmployeeRegisterClient/src/components/Employee.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
+const defaultImageSrc = "/img/profilpn.png";
+
 const initialFieldsValue = {
   employeeId: 0,
   employeeName: "",
   occupation: "",
   imageName: "",
-  imageSrc: "",
+  imageSrc: defaultImageSrc,
   imageFile: null,
 };
 
 const Employee = () => {
   const [value, setValues] = useState(initialFieldsValue);
+  const [error, setError] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -20,13 +23,57 @@ const Employee = () => {
     });
   };
 
+  const showPreview = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let imageFile = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (x) => {
+        setValues({
+          ...value,
+          imageFile: imageFile,
+          imageSrc: x.target.result,
+          imageName: imageFile.name,
+        });
+      };
+      reader.readAsDataURL(imageFile);
+    } else {
+      setValues({
+        ...value,
+        imageFile: null,
+        imageSrc: defaultImageSrc,
+        imageName: "",
+      });
+    }
+  };
+
+  const validate = () => {
+    let temp = {}
+    temp.employeeName = value.employeeName == "" ? false : true;
+    temp.imageSrc = value.imageSrc == defaultImageSrc ? false : true;
+    setError({ ...temp });
+
+    return Object.values(temp).every(x => x===true);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if(validate()){
+      console.log("submitted");
+    }
+  };
+
   return (
     <>
       <div className="container text-center">
         <p className="lead">An Employee</p>
       </div>
 
-      <form autoComplete="off" noValidate>
+      <form
+        autoComplete="off"
+        noValidate
+        className="mb-4"
+        onSubmit={handleFormSubmit}
+      >
         <div className="card">
           <img
             src={value.imageSrc}
@@ -41,7 +88,7 @@ const Employee = () => {
                 accept="image/*"
                 className="form-control-file"
                 name="imageFile"
-                onChange={handleInputChange}
+                onChange={showPreview}
               />
             </div>
 
@@ -67,15 +114,8 @@ const Employee = () => {
               />
             </div>
 
-            <div className="form-group">
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  console.log(value);
-                }}
-              >
+            <div className="form-group text-center">
+              <button type="submit" className="btn btn-primary">
                 Submit
               </button>
             </div>
